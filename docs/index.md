2de5ee4bc3a022a8c9410abcf518a66c26de0bba
Curtis Rueden <ctrueden@wisc.edu>
2021-07-16 15:41:53 -0500

    Add skeleton generated by cookiecutter

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to napari-imagej
+
+Use ImageJ functionality from napari
---
+++ napari_imagej/_tests/test_function.py
@@ -0,0 +1,7 @@
+# from napari_imagej import threshold, image_arithmetic
+
+# add your tests here...
+
+
+def test_something():
+    pass
