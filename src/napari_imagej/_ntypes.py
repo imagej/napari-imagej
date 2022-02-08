6108fcf959e9d875a0a9b8664d6ff4e6ce40fcdc
Gabriel Selzer <gjselzer@wisc.edu>
2022-02-08 15:58:04 -0600

    Clean code

--- src/napari_imagej/_ntypes.py
+++ src/napari_imagej/_ntypes.py
@@ -3,6 +3,9 @@ from labeling.Labeling import Labeling
 
 
 class NapariTypes():
+    """
+    Responsible for conversion between the types normally used by pyimagej and the types normally used by napari.
+    """
     def __init__(self):
         self._to_converters : Dict[Type, Callable] = {}
         self._from_converters : Dict[Type, Callable] = {}
@@ -12,18 +15,23 @@ class NapariTypes():
         self.add_from_converter(Labels, _layer_to_labeling)
 
     def add_to_converter(self, key: Type, value: Callable):
+        """Add a converter from a napari type to a pyimagej type"""
         self._to_converters[key] = value
 
     def add_from_converter(self, key: Type, value: Callable):
+        """Add a converter from a pyimagej type to a napari type"""
         self._from_converters[key] = value
     
     def to_napari(self, object: Any):
+        """Return a version of key that napari prefers"""
         return self._convert(object, self._to_converters)
 
     def from_napari(self, object: Any):
+        """Return a version of key that pyimagej prefers"""
         return self._convert(object, self._from_converters)
 
     def _convert(self, object: Any, map: Dict[Type, Callable]) -> Any:
+        """Return a converted object, if a converter exists for its type"""
         key: Type = type(object)
         if key in map:
             converter = map[key]
@@ -31,11 +39,13 @@ class NapariTypes():
         return object
 
 def _labeling_to_layer(labeling: Labeling):
+    """Converts a Labeling to a Labels layer"""
     img, data = labeling.get_result()
     layer = Labels(img, metadata={"pyLabelingData": data})
     return layer
 
 def _layer_to_labeling(layer: Labels):
+    """Converts a Labels layer to a Labeling"""
     if layer.metadata["pyLabelingData"] is not None:
         metadata = vars(layer.metadata["pyLabelingData"])
         labeling = Labeling(shape=layer.data.shape)
--- src/napari_imagej/_tests/test_ntypes.py
+++ src/napari_imagej/_tests/test_ntypes.py
@@ -1,8 +1,17 @@
+from typing import Any, Dict, List
 import pytest
 import numpy as np
 from napari_imagej._ntypes import _labeling_to_layer, _layer_to_labeling
 from labeling.Labeling import Labeling
+from napari.layers import Labels
 
+def assert_labels_equality(
+    exp: Dict[str, Any], act: Dict[str, Any], ignored_keys: List[str]
+):
+    for key in exp.keys():
+        if key in ignored_keys:
+            continue
+        assert exp[key] == act[key]
 
 @pytest.fixture(scope="module")
 def py_labeling():
@@ -34,9 +43,27 @@ def test_labeling_circular_equality(py_labeling):
 
     assert np.array_equal(exp_img, act_img)
 
-    exp_data = vars(exp_data)
-    print(exp_data)
-    act_data = vars(act_data)
-    print(act_data)
+    assert_labels_equality(
+        vars(exp_data),
+        vars(act_data),
+        ["numSources", "indexImg"]
+    )
 
+def test_labeling_to_labels(py_labeling):
+    """Tests data equality after conversion from labeling to labels"""
+    labels: Labels = _labeling_to_layer(py_labeling)
+    # For a labeling, we need to persist image and metadata
+    exp_img, exp_data = py_labeling.get_result()
+    act_img = labels.data
+    act_data = labels.metadata["pyLabelingData"]
+    assert np.array_equal(exp_img, act_img)
     assert exp_data == act_data
+
+def test_labels_to_labeling(py_labeling):
+    """Tests data equality after conversion from labels to labeling"""
+    labels: Labels = _labeling_to_layer(py_labeling)
+    labeling: Labeling = _layer_to_labeling(labels)
+    # For a labels, we need to persist image
+    exp_img = labels.data
+    act_img, _ = labeling.get_result()
+    assert np.array_equal(exp_img, act_img)
