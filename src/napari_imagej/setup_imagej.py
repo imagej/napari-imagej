ed0eb08e5eebed8cd610a2ac43cd1efdaf0039fd
Gabriel Selzer <gjselzer@wisc.edu>
2022-05-16 11:57:44 -0500

    Switch headless=True to mode='headless'

--- src/napari_imagej/setup_imagej.py
+++ src/napari_imagej/setup_imagej.py
@@ -36,11 +36,9 @@ def imagej_init():
     config.endpoints.append("io.scif:scifio:0.43.1")
 
     log_debug("Completed JVM Configuration")
-    # TODO: change 'headless=True' -> 'mode=imagej.Mode.HEADLESS'
-    # This change is waiting on a new pyimagej release
     # TODO: remove 'add_legacy=False' -> struggles with LegacyService
     # This change is waiting on a new pyimagej release
-    _ij = imagej.init(headless=True)
+    _ij = imagej.init(mode='headless')
 
     log_debug(f"Initialized at version {_ij.getVersion()}")
     return _ij
