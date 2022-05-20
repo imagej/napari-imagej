5032407a39af3279a98fe209e15eb6e23f412292
Gabriel Selzer <gjselzer@wisc.edu>
2022-05-20 12:35:59 -0500

    Simplify when_jvm_starts call

--- src/napari_imagej/__init__.py
+++ src/napari_imagej/__init__.py
@@ -4,4 +4,4 @@ from napari_imagej._napari_converters import init_napari_converters
 from scyjava import when_jvm_starts
 
 # Install napari <-> java converters
-when_jvm_starts(lambda: init_napari_converters())
+when_jvm_starts(init_napari_converters)
