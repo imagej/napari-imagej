28c39dd6d550a2739b45ce7b6b0cb702040f410b
Gabriel Selzer <gjselzer@wisc.edu>
2022-05-20 13:24:22 -0500

    Add some comments clarifying RPC -> Points layer

--- src/napari_imagej/_napari_converters.py
+++ src/napari_imagej/_napari_converters.py
@@ -287,7 +287,12 @@ def _points_to_realpointcollection(points):
 
 
 def _realpointcollection_to_points(collection):
+    # data - collection.size() points, collection.numDimensions() values per point
     data = np.zeros((collection.size(), collection.numDimensions()))
+    # Create a temporary array to pass to each point
+    # N.B. we cannot just write pt.localize(data[i, :]) as JPype does not know
+    # whether to use the localize(double[]) method or the localize(float[]) method.
+    # We thus have to make the decision ourselves using tmp_arr, a double[].
     tmp_arr_dims = int(collection.numDimensions())
     tmp_arr = JArray(JDouble)(tmp_arr_dims)
     for i, pt in enumerate(collection.points()):
