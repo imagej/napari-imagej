a9828ba08507e19d286ea7e33f201c69b16a97f6
Gabriel Selzer <gjselzer@wisc.edu>
2022-09-02 15:05:29 -0500

    Edit main module docstring
    Thanks @ctrueden for the suggestions

--- src/napari_imagej/__init__.py
+++ src/napari_imagej/__init__.py
@@ -1,21 +1,20 @@
 """
-napari-imagej provides a graphical wrapper for the PyImageJ project, exposed as a
-plugin for the napari application.
+napari-imagej brings the power of ImageJ, ImageJ2, and Fiji to the napari viewer.
 
-There are two advantages to using napari-imagej over PyImageJ. The obvious advantage
-is the graphical interface, easing the use of PyImageJ functionality.
-
-The secondary advantage of napari-imagej is its ability to automatically convert
-data between equivalent Java and Python data types;
-the plugin assumes the responsibility of converting napari Layers, numpy arrays,
-Python primitives, etc. into Java equivalents right before any calls to ImageJ2
-(Java) functionality, and takes care to convert any Java outputs back into Python
-for display.
+With napari-imagej, users can call headless functionality from any of these applications
+on napari data structures. Users can also call SciJava scripts on data in napari,
+automatically discoverable within the plugin. Users can launch the ImageJ or ImageJ2
+user interfaces from napari-imagej and can explicitly transfer data to and from the
+napari user interface. Most importantly, all of this functionality is accessible WITHOUT
+any explicit conversion between the two ecosystems!
 
 napari-imagej is NOT designed to be used on its own; instead, it should be launched
 from within the napari application. Please see (https://napari.org/stable/#installation)
 to get started using the napari application. Once napari is installed, you can then
 add napari-imagej as a plugin. Please see (https://www.napari-hub.org/) for a list
 of available plugins, including napari-imagej.
+
+napari-imagej is built upon the PyImageJ project:
+https://pyimagej.readthedocs.io/en/latest/
 """
 __version__ = "0.0.1.dev0"
