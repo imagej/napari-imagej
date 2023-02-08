bb2e8576733c967d39a49bda2fba28f3452e5b3a
Gabriel Selzer <gjselzer@wisc.edu>
2023-02-08 10:12:54 -0600

    Initialize ReadTheDocs

---
+++ doc/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src/napari-imagej"))
+
+project = "napari-imagej"
+copyright = "2023, ImageJ2 developers"
+author = "ImageJ2 developers"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinx_rtd_theme"
---
+++ doc/index.rst
@@ -0,0 +1,24 @@
+.. napari-imagej documentation master file, created by
+   sphinx-quickstart on Tue Feb  7 14:11:33 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to napari-imagej's documentation!
+=========================================
+
+napari-imagej aims to provide access to all `ImageJ2 <https://imagej.net/software/imagej2>`_ functionality through the `napari <https://github.com/napari/napari>`_ graphical user interface.
+
+napari-imagej builds on the foundation of `PyImageJ <https://github.com/imagej/pyimagej>`_, a project allowing ImageJ2 access from Python.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
