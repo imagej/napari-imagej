f7edb19b37cfb2415d3c7e77a6a81d0f6552bb2a
Curtis Rueden <ctrueden@wisc.edu>
2023-05-04 12:55:25 -0500

    Add test to print out Java component versions
    It doesn't explicitly assert anything (yet), but it's nice to
    see what versions of things are being used when the tests run.

---
+++ tests/test_java.py
@@ -0,0 +1,28 @@
+from napari_imagej.java import jc
+from scyjava import get_version, jimport
+
+
+def test_java_components(ij):
+    """
+    Assert that Java components are present, and print their versions.
+    """
+    version_checks = {
+        "net.imagej:imagej-common": "net.imagej.Dataset",
+        "net.imagej:imagej-ops": "net.imagej.ops.OpService",
+        "net.imglib2:imglib2-unsafe": "net.imglib2.img.unsafe.UnsafeImg",
+        "net.imglib2:imglib2-imglyb": "net.imglib2.python.ReferenceGuardingRandomAccessibleInterval",
+        "org.scijava:scijava-common": "org.scijava.Context",
+        "org.scijava:scijava-search": "org.scijava.search.Searcher",
+        "net.imagej:imagej-legacy": "net.imagej.legacy.LegacyService",
+        "net.imagej:ij": "ij.ImagePlus",
+    }
+    print()
+    print("======= BEGIN JAVA VERSIONS =======")
+    for coord, class_name in version_checks.items():
+        try:
+            jcls = jimport(class_name)
+        except Exception:
+            jcls = None
+        version = "NOT PRESENT" if jcls is None else get_version(jcls)
+        print(f"{coord} {version}")
+    print("======== END JAVA VERSIONS ========")
