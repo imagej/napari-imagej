193180d6384464384bfde9a7cb7b26344906244c
Gabriel Selzer <gjselzer@wisc.edu>
2024-10-15 09:56:37 -0500

    Use Ruff!

--- bin/lint.sh
+++ bin/lint.sh
@@ -4,11 +4,9 @@ dir=$(dirname "$0")
 cd "$dir/.."
 
 exitCode=0
-black src tests
+ruff check
 code=$?; test $code -eq 0 || exitCode=$code
-isort src tests
-code=$?; test $code -eq 0 || exitCode=$code
-python -m flake8 src tests
+ruff format --check
 code=$?; test $code -eq 0 || exitCode=$code
 validate-pyproject pyproject.toml
 code=$?; test $code -eq 0 || exitCode=$code
